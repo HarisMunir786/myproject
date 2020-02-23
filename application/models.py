29c547ef8e4f2722da357b25119360ddcdfec633
Haris Munir <Haris.Munir@qa.com>
2020-02-23 14:18:41 +0000

    Amendements done. created edit.html,posts.html, changes layout.html, register.html, books.html, home.html,login.html

--- application/models.py
+++ application/models.py
@@ -3,49 +3,46 @@ from application import login_manager
 from flask_login import UserMixin
 from datetime import datetime
 
+
+class BlogPost(db.Model):
+    blogPost_id = db.Column(db.Integer, primary_key=True)
+    blogPost_title = db.Column(db.String(100), nullable=False, unique=True)
+    blogPost_content = db.Column(db.Text, nullable=False)
+    blogPost_date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    def __repr__(self):
+        return ''.join([
+			'User ID: ', str(self.id), '\r\n',
+			'Title: ', self.title, '\r\n', self.content
+		])
+
 @login_manager.user_loader
 def load_user(id):
     return Users.query.get(int(id))
 
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False, unique=True)
-    content = db.Column(db.String(500), nullable=False, unique=True)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    def __repr__(self):
-        return ''.join([
-            'User: ', self.first_name, ' ', self.last_name, '\r\n',
-            'Title: ', self.title, '\r\n', self.content
-            ])
-
-
-
 class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
-    posts = db.relationship('Posts', backref='author', lazy=True)
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
+    books = db.relationship('Books', backref='book_ref', lazy=True)
+    def __repr__(self):
+        return ''.join([
+            'User ID: ', str(self.id), '\r\n',
+            'Email: ', self.email, '\r\n',
+            ])
+
+class Books(db.Model, UserMixin):
+    book_id = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String(30), nullable=False)
+    book_author = db.Column(db.String(30), nullable=False)
+    book_genre = db.Column(db.String(150), nullable=False, unique=True)
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
     books= db.relationship('Books', backref='book_ref', lazy=True)
     def __repr__(self):
         return ''.join([
             'User ID: ', str(self.id), '\r\n',
             'Email: ', self.email, '\r\n',
-            'Name: ', self.first_name, ' ', self.last_name
-        ])
-
-class Books(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   book_id= db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
-   title = db.Column(db.String(100), nullable=False)
-   author = db.Column(db.String(30), nullable=False)
-   genre = db.Column(db.String(30), nullable=False)
-   def __repr__(self):
-        return ''.join([
-            'ISBN: ', self.id, '\r\n',
-            'Title: ', self.title, '\r\n',
-	    'Author: ', self.author, '\r\n',
-	    'Genre: ', self.genre,
             ])
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from application import app
+from flask import render_template, request, redirect
+from application import app, db
+from application.models import BlogPost, Users, Books
+from application.forms import PostForm, LoginForm, UpdateAccountForm, RegistrationForm
 
 @app.route('/')
 @app.route('/home')
@@ -10,18 +12,71 @@ def home():
 def books():
      return render_template('books.html', title='Books')
 
-@app.route('/register')
+@app.route('/register', methods = ['GET','POST'])
 def register():
+     form = RegistrationForm()
+     if form.validate_on_submit():
+        register_data = Users(
+        first_name = form.first_name.data,
+        last_name = form.last_name.data,
+        email = form.email.data,
+        password = form.password.data
+    )
+     db.session.add(register_data)
+     db.session.commit()
      return render_template('register.html', title='Register')
 
 @app.route('/login')
 def login():
      return render_template('login.html', title='Login')
 
-@app.route('/myaccount')
+@app.route('/login/myaccount')
 def myaccount():
      return render_template('myaccount.html', title='My Account')
 
-@app.route('/logout')
+@app.route('/login/myaccount/logout')
 def logout():
      return render_template('logout.html', title='Logout')
+
+@app.route('/login/myaccount/posts', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/login/myaccount/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('/login/myaccount/posts', all_posts=BlogPost)
+
+@app.route('/login/myaccount/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/login/myaccount.html')
+
+@app.route('/login/myaccount/posts/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST': 
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=BlogPost)
+
+@app.route('/login/myaccount/admin', methods=['GET','POST'])
+def bookentry(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST': 
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
